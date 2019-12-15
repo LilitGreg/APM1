8ecd8734e8f080fcb0648bff495ad0152ffeec12
Lilit Grigoryan <lilit.grigoryan@accenture.com>
2019-12-15 16:50:53 +0200

    beforeresolver

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,28 @@
+import { ProductResolver } from './product-resolver.service';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild([
+      {path: 'products' , component: ProductListComponent},
+      {path: 'products/:id' , component: ProductDetailComponent,
+      resolve: {resolvedDate: ProductResolver } },
+      {path: 'products/:id/edit' , component: ProductEditComponent,
+      resolve: {resolvedDate: ProductResolver } }
+    ])
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductEditComponent
+  ]
+})
+export class ProductModule { }
